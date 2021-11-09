4e00419378a85ee9558aa920d649eb0fcecef687
Marcin <mwasyl@gmail.com>
2021-11-09 20:02:46 +0100

    commands and command handlers

---
+++ src/Services/Ordering/Ordering.Application/Exceptions/ValidationException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using FluentValidation.Results;
+
+namespace Ordering.Application.Exceptions
+{
+    public class ValidationException : ApplicationException
+    {
+        public ValidationException()
+            :base("One or more validation failures have occured.")
+        {
+            Errors = new Dictionary<string, string[]>();
+        }
+
+        public ValidationException(IEnumerable<ValidationFailure> failures)
+            :this()
+        {
+            Errors = failures.GroupBy(p => p.PropertyName, e => e.ErrorMessage)
+                .ToDictionary(failureGroup => failureGroup.Key, failureGroup => failureGroup.ToArray());
+        }
+        
+        public Dictionary<string,string[]> Errors { get; }
+    }
+}
